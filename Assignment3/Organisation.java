51da25ae1a7eac1d4e1e194784f794af696ad328
Pannaga Sayen Chikkam <149186165+pannagachikkam@users.noreply.github.com>
2024-04-13 13:47:38 +0530

    Create Organisation.java

---
+++ Assignment3/Organisation.java
@@ -0,0 +1,53 @@
+class Organization implements Cloneable {
+    private int organizationCode;
+    private String organizationName;
+    private String organizationAddress;
+
+    public Organization(int organizationCode, String organizationName, String organizationAddress) {
+        this.organizationCode = organizationCode;
+        this.organizationName = organizationName;
+        this.organizationAddress = organizationAddress;
+    }
+
+    public int getOrganizationCode() {
+        return organizationCode;
+    }
+
+    public String getOrganizationName() {
+        return organizationName;
+    }
+
+    public String getOrganizationAddress() {
+        return organizationAddress;
+    }
+
+    public void printDetails() {
+        System.out.println("Organization Code: " + organizationCode);
+        System.out.println("Organization Name: " + organizationName);
+        System.out.println("Organization Address: " + organizationAddress);
+    }
+
+    @Override
+    public Organization clone() {
+        try {
+            return (Organization) super.clone();
+        } catch (CloneNotSupportedException e) {
+            // This should never happen since Organization implements Cloneable
+            throw new InternalError(e);
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Organization org1 = new Organization(002744, "MCOI", " HYDERABAD");
+
+        Organization org2 = org1.clone();
+
+        System.out.println("Original Organization:");
+        org1.printDetails();
+
+        System.out.println("\nCloned Organization:");
+        org2.printDetails();
+    }
+}
